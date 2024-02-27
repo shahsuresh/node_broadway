fc181623d19ad23483a2eeb3a17aef6b774526ac
shahsuresh <shahsuresh75@gmail.com>
2024-02-27 13:33:32 +0545

    edit operation addes

--- index.js
+++ index.js
@@ -82,6 +82,54 @@ app.delete("/course/delete/:id", async (req, res) => {
   res.status(200).send({ message: "Course Deleted Successfully" });
 });
 
+//? edit course details
+
+app.put("/course/edit/:id", async (req, res) => {
+  // extract courseId from req.params
+
+  const courseId = req.params.id;
+  console.log(courseId);
+
+  // check for mongo id validity
+  const isValidMongoId = mongoose.isValidObjectId(courseId);
+  console.log(isValidMongoId);
+  // if not valid, throw error
+  if (!isValidMongoId) {
+    return res.status(400).send({ message: "not valid mongo ID" });
+  }
+  // find course by id
+  const requiredCourse = await Course.findOne({ _id: courseId });
+  console.log(requiredCourse);
+  // if not course, throw error
+
+  if (!requiredCourse) {
+    return res.status(404).send({ message: "Course does not exist" });
+  }
+
+  // get new values from req.body
+
+  const newValues = req.body;
+  //console.log(newValues);
+
+  // edit product
+
+  await Course.updateOne(
+    { _id: courseId },
+    {
+      $set: {
+        name: newValues.name,
+        price: newValues.price,
+        tutorName: newValues.tutorName,
+        duration: newValues.duration,
+      },
+    }
+  );
+
+  // send response
+
+  return res.status(200).send({ message: "Edited successfully" });
+});
+
 //? Assigning port to app and creating server=========
 const PORT = 4000;
 app.listen(PORT, () => {
